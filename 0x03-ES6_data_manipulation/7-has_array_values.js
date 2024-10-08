12983c3c0125b4e43cc784053c615cff2caffed5
Ayokayzy <theayokayzy1@gmail.com>
2024-10-08 22:46:06 +0100

    more tasks

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+export default function hasValuesFromArray(set, array) {
+  if (set instanceof Set && Array.isArray(array)) {
+    const initial = true;
+    const value = array.reduce(
+      (previous, current) => previous && set.has(current),
+      initial
+    );
+
+    return value;
+  }
+  return false;
+}
